16dd72c9995266fba665deadb86a2d870da4b461
minucedev <ngnmihpuc.1502@gmail.com>
2025-09-23 21:27:26 +0700

    first commit

---
+++ src/main/java/com/minucecoding/brief_ai_with_spring/BriefAiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.minucecoding.brief_ai_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefAiWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefAiWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minucecoding/brief_ai_with_spring/BriefAiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minucecoding.brief_ai_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefAiWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
